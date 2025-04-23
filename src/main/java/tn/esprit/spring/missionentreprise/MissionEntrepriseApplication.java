a2d8018bdb408ef85616b935d4bf785c573bb1b3
Yasmine <jebali.yasmine.1@esprit.tn>
2025-04-23 23:04:14 +0200

    Projet Spring Vierge

---
+++ src/main/java/tn/esprit/spring/missionentreprise/MissionEntrepriseApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring.missionentreprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissionEntrepriseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MissionEntrepriseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/spring/missionentreprise/MissionEntrepriseApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring.missionentreprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionEntrepriseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
