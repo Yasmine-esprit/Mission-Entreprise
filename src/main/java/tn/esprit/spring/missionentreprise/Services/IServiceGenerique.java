87cd4c630023a4592876eb4bdf7e8beb9e0bb3e1
mariem oueslati <mariemoueslati165@gmail.com>
2025-04-30 16:02:54 +0100

    association + repo module user + messagerie completed

--- src/main/java/tn/esprit/spring/missionentreprise/Services/IServiceGenerique.java
+++ src/main/java/tn/esprit/spring/missionentreprise/Services/IServiceGenerique.java
@@ -1,4 +1,22 @@
 package tn.esprit.spring.missionentreprise.Services;
 
-public interface IServiceGenerique {
+import java.util.List;
+
+public interface IServiceGenerique<T> {
+
+    T add(T t);
+    List<T> addAll(List<T> t);
+    List<T> getAll();
+    T edit(T t);
+    List<T> editAll(List<T> t);
+    void delete(T t);
+    void deleteAll();
+    void deleteById(Long id);
+
+
+
+    T getById(Long id);
+    boolean existsById(Long id);
+    Long count();
+
 }
