b339ba42a2bcf67a872c210b415fc76234d9d5bf
Yasmine <jebali.yasmine.1@esprit.tn>
2025-07-02 08:07:54 +0200

    pulling

--- src/main/java/tn/esprit/spring/missionentreprise/Controllers/CritereRestController.java
+++ src/main/java/tn/esprit/spring/missionentreprise/Controllers/CritereRestController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 public class CritereRestController {
 
+
     private final CritereService critereService;
 
     @PostMapping
@@ -63,7 +64,7 @@ public class CritereRestController {
     public ResponseEntity<String> deleteCritere(@PathVariable Long id) {
         try {
             critereService.deleteById(id);
-            return ResponseEntity.ok("Critere deleted successfully");
+            return ResponseEntity.ok("Criterniahauyhe deleted successfully idk what im ttypping but im trring my best to write sth for ");
         } catch (Exception e) {
             return ResponseEntity.internalServerError()
                     .body("Error deleting critere: " + e.getMessage());
